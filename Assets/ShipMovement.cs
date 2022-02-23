30f3d79a079d050c13f667306006704452d2e46e
Keane <KeaneCarotenuto@gmail.com>
2022-02-23 19:22:34 +1300

    Adding Input Action

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShipMovement : MonoBehaviour
 {
+    public InputAction movementAction;
+    public InputAction boostAction;
+    public InputAction lookAction;
+
+    public float forwardSpeed = 10f;
+    public float turnSpeed = 10f;
+    public float rollSpeed = 10f;
+    public float strafeSpeed = 10f;
+    public float verticalSpeed = 10f;
+    public float maxSpeed = 10f;
+    public float minSpeed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +26,16 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        KeyboardMovement();
     }
 
     public void KeyboardMovement() {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Forward");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical, forward);
+
+        transform.Translate(movement * Time.deltaTime);
     }
 }

9c2308ca0a1c8dcfe68b72298e81f21c47864267
Nerys-Thamm <nerysthamm@mgail.com>
2022-02-24 14:28:57 +1300

    skybox and planet

--- Assets/Scripts/Projectile_Weapon.cs
+++ Assets/Scripts/Projectile_Weapon.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Projectile_Weapon : Ranged_Weapon
 {
     public GameObject projectilePrefab;
-    
+    public AudioSource fireSource;
     
     
     public override void Fire()
     {
         GameObject projectile = Instantiate(projectilePrefab, muzzlePositions[currentMuzzle].position, muzzlePositions[currentMuzzle].rotation);
         projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectileSpeed;
+        fireSource.Play();
         Destroy(projectile, 5.0f);
     }
 }

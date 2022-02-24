fda114873016ffca7b0780a5852617e615384330
Nerys-Thamm <nerysthamm@mgail.com>
2022-02-24 23:55:10 +1300

    Made an AI for ships

--- Assets/Scripts/AI/SpaceshipNavigationAI.cs
+++ Assets/Scripts/AI/SpaceshipNavigationAI.cs
@@ -4,6 +4,76 @@ using UnityEngine;
 
 public class SpaceshipNavigationAI : MonoBehaviour
 {
+    Vector3 flightVector;
+    public Transform targetPosition;
+
+    
+
+    public float avoidanceRange;
+
+    //Set Flight Vector
+    void SetFlightVector()
+    {
+        if((targetPosition.position - transform.position).magnitude < avoidanceRange) return;
+        flightVector = targetPosition.position - transform.position;
+    }
+
+    
+    void RollToFlightVector()
+    {
+        Vector3 rollVector = Vector3.ProjectOnPlane(flightVector, transform.forward).normalized;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.forward, rollVector), Time.deltaTime * 1.0f);
+    }
+
+    void PitchToFlightVector()
+    {
+        
+    }
+    
+
+    void RotateToFlightVector()
+    {
+        Vector3 rollVector = Vector3.ProjectOnPlane(flightVector, transform.forward).normalized;
+        Vector3 pitchVector = Vector3.ProjectOnPlane(flightVector, transform.right).normalized;
+        Vector3 yawVector = Vector3.ProjectOnPlane(flightVector, transform.up).normalized;
+
+        float dot = Vector3.Dot(transform.up, rollVector);
+
+        Quaternion roll = Quaternion.LookRotation(transform.forward, rollVector);
+        Quaternion pitch = Quaternion.LookRotation(pitchVector, Vector3.Cross(pitchVector, transform.right));
+        Quaternion yaw = Quaternion.LookRotation(yawVector, transform.up);
+
+        Quaternion yawPitch = Quaternion.Slerp(yaw, pitch, Mathf.Abs(dot));
+
+
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,  Quaternion.Slerp(yawPitch,roll,0.5f), Time.deltaTime * 3.0f);
+    }
+
+
+
+    //Detect if there is an obstacle in the way of the flight vector and adjust the flight vector to steer away from it
+    void AvoidObstacles()
+    {
+        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, flightVector, out hit, avoidanceRange))
+        {
+            //If the raycast hit something, adjust the flight vector to steer away from it
+            flightVector = (Vector3.Reflect(flightVector, hit.normal) + flightVector);
+        }
+        
+        
+    }
+
+    
+
+    public void SetTarget(Transform target)
+    {
+        targetPosition = target;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +83,27 @@ public class SpaceshipNavigationAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetFlightVector();
+        AvoidObstacles();
+        RotateToFlightVector();
+        //Move forward
+        transform.position += transform.forward * Time.deltaTime * 20.0f;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + flightVector);
+
+        Vector3 rollVector = Vector3.ProjectOnPlane(flightVector, transform.forward).normalized;
+        Vector3 pitchVector = Vector3.ProjectOnPlane(flightVector, transform.right).normalized;
+        Vector3 yawVector = Vector3.ProjectOnPlane(flightVector, transform.up).normalized;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + rollVector*2.0f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + pitchVector*2.0f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + yawVector*2.0f);
     }
 }

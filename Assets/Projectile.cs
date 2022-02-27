c574cd41e9120ed0cde39ffa75b157823959c811
Nerys-Thamm <nerysthamm@mgail.com>
2022-02-28 12:49:36 +1300

    Enemy indicators done

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class Projectile : MonoBehaviour
 {
 
     public GameObject hitPrefab;
+
+    public Material[] decals;
 
     public float lifetime;
     // Start is called before the first frame update
@@ -39,6 +42,7 @@ public class Projectile : MonoBehaviour
         
         GameObject hit = Instantiate(hitPrefab, transform.position, Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal));
         hit.transform.parent = collision.transform;
+        hit.GetComponentInChildren<DecalProjector>().material = decals[Random.Range(0, decals.Length)];
         Destroy(hit, 20.0f);
         Destroy(gameObject);
     }

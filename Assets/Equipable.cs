b3a041a6f70cdc907f03889272da24c91410ff50
Keane <KeaneCarotenuto@gmail.com>
2022-03-02 02:11:20 +1300

    Half complete weapon equip system

---
+++ Assets/Equipable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipable : MonoBehaviour
+{
+    public enum EquipableType
+    {
+        rangedWeapon,
+        equipment
+    }
+
+    public EquipableType type = EquipableType.rangedWeapon;
+    public bool isEquipped = false;
+
+    public HardpointManager _hardpointManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_hardpointManager == null)
+        {
+            _hardpointManager = FindObjectOfType<HardpointManager>();
+        }
+
+        if (_hardpointManager != null)
+        {
+            _hardpointManager.AddEquipable(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Equip(Transform hardpoint)
+    {
+        if (isEquipped)
+        {
+            return;
+        }
+
+        transform.parent = hardpoint;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        isEquipped = true;
+    }
+
+    public void Unequip()
+    {
+        if (!isEquipped)
+        {
+            return;
+        }
+
+        transform.parent = null;
+        isEquipped = false;
+    }
+}
